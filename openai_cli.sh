6b06ef2a0a9c7a755407a3e43560d1f887c0d8a4
Aimnexus <madelene.lidmalm@gmail.com>
2025-04-07 02:44:48 +0200

    Update openai_cli.sh

--- openai_cli.sh
+++ openai_cli.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PROMPT=$1
-API_sk-proj-BJC9HkahgMlOLl9e3F-R9P789V1BvvWMwoh12_wyll-G7fnGHkpfBNiXVjlgF-Qnvr0oVGdekhT3BlbkFJwhjm_YyviYKSRAxSpGa3PAMWuvIIOkbpHZCWsjn4Zp2K8lJt71tM2-QgCBmOKYbGUenLXm3lEA
+API_sk-proj-BJC9HkahgMlOLl9e3F-Qnvr0oVGdekhT3BlbkFJwhjm_-QgCBmOKYbGUenLXm3lEA
 
 
 curl https://api.openai.com/v1/chat/completions \
